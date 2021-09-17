21a66dcb00283d1d007f5edae26e5f4da25fe103
thegruffalo <thegruffalo@users.noreply.github.com>
2021-09-17 19:08:15 +0100

    Create snack_bodyweight.js

---
+++ src/js/routines/snack_bodyweight.js
@@ -0,0 +1,67 @@
+export default
+{
+    "name": "Fitness Snack bodyweight",
+    "sub_routines": [
+        {
+            "name": "Workout",
+            "intervals": [
+                {
+                    "name": "Crunches",
+                    "duration": 30
+                },
+                {
+                    "name": "Rest",
+                    "duration": 10
+                },
+                {
+                    "name": "Slow Squats",
+                    "duration": 30
+                },
+                {
+                    "name": "Rest",
+                    "duration": 10
+                },
+                {
+                    "name": "Walk out w push up",
+                    "duration": 30
+                },
+                {
+                    "name": "Rest",
+                    "duration": 10
+                },
+                {
+                    "name": "Plank",
+                    "duration": 30
+                },
+                {
+                    "name": "Rest",
+                    "duration": 5
+                },
+                {
+                    "name": "Plank Left",
+                    "duration": 30
+                },
+                {
+                    "name": "Rest",
+                    "duration": 10
+                },
+                {
+                    "name": "Plank Right",
+                    "duration": 30
+                },
+                {
+                    "name": "Rest",
+                    "duration": 5
+                },
+                {
+                    "name": "Push ups",
+                    "duration": 30
+                }
+            ],
+            "sets": 2,
+            "duration_between_sets": 10,
+            "start_delay": 5,
+            "end_delay": 0
+        }
+    ]
+};
